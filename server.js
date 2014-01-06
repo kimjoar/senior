23577d28fba173b47d5d4829d676d2c6fd9abe13
Bendik Solheim <bendik.solheim@bekk.no>
2014-01-06 12:01:58 +0100

    Forgot file..

--- server.js
+++ server.js
@@ -1,67 +1,30 @@
 var express = require('express');
-var request = require('request');
+var socialcast = require('./socialcast');
 var app = express();
-
-var socialcastUrl = process.env.SOCIALCAST_URL;
-var socialcastUser = process.env.SOCIALCAST_USER;
-var socialcastPassword = process.env.SOCIALCAST_PASS;
-
-var socialcastAuth = {
-    user: socialcastUser,
-    pass: socialcastPassword
-};
-
-function messages() {
-    var messages = [];
-
-    setInterval(function() {
-        messages = [];
-    }, 120000);
-
-    return function(newMessages) {
-        if (newMessages) {
-            messages = newMessages;
-        }
-        return messages;
-    }
-}
-
-var socialcastMessages = messages();
-
-function socialcastParams(url) {
-    return {
-        url: socialcastUrl + url,
-        json: true,
-        auth: socialcastAuth
-    };
-}
 
 app.get('/messages', function(req, res) {
 
-    if (socialcastMessages().length > 0) return res.json(socialcastMessages());
+    socialcast.messages(function(error, messages) {
+        if (error) return handleError(error);
 
-    request.get(socialcastParams('/api/messages'), function(error, response, body) {
-        if (error) {
-            return handleError(error);
-        }
-        res.json(socialcastMessages(body));
+        res.json(messages);
     });
 
 });
 
 app.get('/message/:id', function(req, res) {
 
-    request.get(socialcastParams('/api/messages/' + req.params.id), function(error, response, body) {
-        if (error) {
-            return handleError(error);
-        }
-        res.json(body);
+    socialcast.message(req.params.id, function(error, message) {
+        if (error) return handleError(error);
+
+        res.json(message);
     });
 
 });
 
 function handleError(err) {
     console.log("an error has occured. keep calm and carry on.");
+    console.error(err);
 }
 
 // if on heroku use heroku port.
