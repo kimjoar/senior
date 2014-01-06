0b640a3339b3452a2abd97a5796f56e3218ad53a
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-01-06 11:02:04 +0100

    first try

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
+var socialcastUrl = process.env.SOCIALCAST_URL;
+var socialcastUser = process.env.SOCIALCAST_USER;
+var socialcastPassword = process.env.SOCIALCAST_PASS;
+
 var demo_url = "https://api.github.com/users/bekkopen/repos";
 
 app.get('/', function(req, res) {
@@ -13,11 +17,46 @@ app.get('/', function(req, res) {
         }
     }, function(error, response, body) {
         if (error) {
-            console.log("an error has occured. keep calm and carry on.");
+            return handleError(error);
         }
         res.json(body);
     });
 });
+
+app.get('/messages', function(req, res) {
+
+    request.get({
+        url: socialcastUrl + '/api/messages',
+        json: true,
+        'auth': {
+            'user': socialcastUser,
+            'pass': socialcastPassword
+        }
+    }, function(error, response, body) {
+        if (error) {
+            return handleError(error);
+        }
+
+        request.get({
+            url: socialcastUrl + '/api/messages?page=2',
+            json: true,
+            'auth': {
+                'user': socialcastUser,
+                'pass': socialcastPassword
+            }
+        }, function(error, response, body2) {
+            if (error) {
+                return handleError(error);
+            }
+            res.json(body.concat(body2));
+        });
+    });
+
+});
+
+function handleError(err) {
+    console.log("an error has occured. keep calm and carry on.");
+}
 
 
 // if on heroku use heroku port.
